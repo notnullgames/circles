79717ce707a6e86c94cedd993df8d587934c283f
David Konsumer <konsumer@jetboystudio.com>
2014-12-18 20:51:27 -0800

    still broke, tablingfor now

--- src/game.js
+++ src/game.js
@@ -1,25 +1,35 @@
 var PIXI = require('pixi.js');
 var ajax = require('superagent');
+var svgToSprites = require('./svgToSprites.js');
 
 var stage = new PIXI.Stage(0);
 var renderer = PIXI.autoDetectRenderer(800, 600);
 document.body.appendChild(renderer.view);
 var startTime = (new Date()).getTime();
+var layers;
 
-var svgToSprites = require('./svgToSprites.js');
 
 ajax.get('assets/levels/level1.svg')
   .end(function(res){
     var SVG = new DOMParser().parseFromString(res.text, 'text/xml');
-    svgToSprites(SVG).forEach(function(l){
-      stage.addChild(l);
-    });
+    layers = svgToSprites(SVG);
+    for (var i in layers){
+      stage.addChild(layers[i]);
+    }
+    console.log(layers);
+
   });
 
 
 function animate() {
   requestAnimationFrame( animate ); 
   renderer.render(stage);
+  if (layers && layers.goal){
+    layers.goal.children.forEach(function(enemy){
+      enemy.x -= 1;
+      enemy.y += 1;
+    })
+  }
 }
 animate();
 
--- src/svgToSprites.js
+++ src/svgToSprites.js
@@ -1,6 +1,6 @@
 var PIXI = require('pixi.js');
 
-// create a PIXI.DisplayObjectContainer for each layer
+// create a PIXI.DisplayObjectContainer for each inkscape layer
 // convert each top-level object in layer to a PIXI.Sprite
 module.exports = function(SVG){
   var svgGroups = SVG.getElementsByTagName("g");
@@ -8,33 +8,35 @@ module.exports = function(SVG){
   var vals = [];
   var i, s;
   for (i in svgGroups){
-    if(svgGroups[i] && svgGroups[i].attributes){
+    console.log(svgGroups.item(i).parentNode.tagName);
+    if (svgGroups.item(i).parentNode.tagName != 'svg') continue;
+    var item = svgGroups.item(i);
+    if(svgGroups[i] && item.attributes){
       name = 'layer' + i;
-      if (svgGroups[i].attributes['inkscape:label']){
-        name = svgGroups[i].attributes['inkscape:label'].value;
-      }else if (svgGroups[i].attributes.id){
-        name = svgGroups[i].attributes.id.value;
+      if (item.attributes['inkscape:label']){
+        name = item.attributes['inkscape:label'].value;
+      }else if (item.attributes.id){
+        name = item.attributes.id.value;
       }
       if (keys.indexOf(name) === -1){
         keys.push(name);
-        vals.push(svgGroups[i]);
+        vals.push(item);
       }
     }
   }
-  var containers = [];
-  var header = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="800" height="600" version="1.1">';
+  var containers = {};
+  var header = '<?xml version="1.0" encoding="UTF-8"?><svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="800" height="600" version="1.1">';
   for (i in keys){
     var group = vals[i];
     var layerName = keys[i];
     var container = new PIXI.DisplayObjectContainer();
-
-    var svgSprites = group.getElementsByTagName("*");
-    for (i in svgSprites){
-      var str = 'data:image/svg+xml;utf8,' + encodeURIComponent(header + svgSprites.item(i).outerHTML + '</svg>');
+    var svgSprites = group.querySelectorAll("*");
+    for (s in svgSprites){
+      var str = 'data:image/svg+xml;utf8,' + encodeURIComponent(header + svgSprites.item(s).outerHTML + '</svg>');
       var sprite = PIXI.Sprite.fromImage(str);
       container.addChild(sprite);
     }
-    containers.push(container);
+    containers[layerName] = container;
   }
   return containers;
 }
